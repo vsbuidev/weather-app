7ad0b54d655e976234e394b8895eeebc996078d8
vsbuidev <vsb.uidev@gmail.com>
2024-01-18 17:06:43 +0530

    Final commit

--- scripts.js
+++ scripts.js
@@ -13,14 +13,41 @@ function getWeather() {
     .then((response) => response.json())
     .then((data) => {
       const weatherInfo = document.getElementById("weather-info");
-      const temperature = (data.main.temp - 273.15).toFixed(2); // Convert Kelvin to Celsius
+      const temperature = (data.main.temp - 273.15).toFixed(2);
       const description = data.weather[0].description;
+      const windSpeed = data.wind.speed;
+      const humidity = data.main.humidity;
       const cityName = data.name;
 
+      let temperatureIcon = "fas fa-thermometer-half";
+      let descriptionIcon = "fas fa-sun";
+      let windIcon = "fas fa-wind";
+      let humidityIcon = "fas fa-tint";
+
+      if (temperature < 10) {
+        temperatureIcon = "fas fa-snowflake";
+      } else if (temperature > 30) {
+        temperatureIcon = "fas fa-sun";
+      }
+
+      if (description.includes("rain")) {
+        temperatureIcon = "fas fa-cloud-showers-heavy";
+      } else if (description.includes("cloud")) {
+        temperatureIcon = "fas fa-cloud";
+      }
+
+      if (humidity > 80) {
+        humidityIcon = "fas fa-water";
+      } else {
+        humidityIcon = "fas fa-tint";
+      }
+
       weatherInfo.innerHTML = `
-          <h2>${cityName}</h2>
-          <p>Temperature: ${temperature} &#8451;</p>
-          <p>Description: ${description}</p>
+          <h3>${cityName}</h3>
+          <p><i class="${temperatureIcon}"></i> ${temperature} &#8451;</p>
+          <p><i class="${descriptionIcon}"></i> ${description}</p>
+          <p><i class="${windIcon}"></i> ${windSpeed} m/s</p>
+          <p><i class="${humidityIcon}"></i> ${humidity}%</p>
         `;
     })
     .catch((error) => {
